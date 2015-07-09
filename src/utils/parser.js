3a8d2c0dfb33c79555e3ead43f29d4d7fa5b7f84
kicumkicum <kicumkicum@gmail.com>
2015-07-10 01:11:19 +0600

    update parser

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,9 +1,9 @@
-/**
- * Created by oleg on 08.07.15.
- */
-Parser = {};
+goog.provide('utils.Parser');
 
-Parser.getTiket = function(url) {
+
+utils.Parser = function() {};
+
+utils.Parser.getTicket = function(url) {
 	var mask = 'dev.ifaced.ru/issues/';
 	var pos = url.indexOf(mask) + mask.length;
 	var ticket = '';
@@ -13,11 +13,10 @@ Parser.getTiket = function(url) {
 	return ticket;
 };
 
-Parser.getBranch = function(url) {
+utils.Parser.getBranch = function(url) {
 	return 'localhost';
 };
 
-Parser.getPull = function(url) {
-
+utils.Parser.getPull = function(url) {
 	return 'localhost';
 };
