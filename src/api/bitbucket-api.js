6e0241c91b43580f6c487fdc0e13de86ee507d74
kicumkicum <kicumkicum@gmail.com>
2015-07-10 23:35:07 +0600

    api.Bitbucket.prototype.getPullRequests use recursion for get all pull-requests

--- src/api/bitbucket-api.js
+++ src/api/bitbucket-api.js
@@ -10,7 +10,8 @@ goog.require('models.bitbucket.PullRequest');
 api.Bitbucket = function() {
 	this._url = 'https://bitbucket.org/api/2.0/';
 	this._owner = 'interfaced';
-	this._repoSlug = 'persik.by'
+	this._repoSlug = 'persik.by';
+	this._maxPageLength = 50;
 
 };
 goog.inherits(api.Bitbucket, api.AbstractApi);
@@ -20,12 +21,30 @@ goog.inherits(api.Bitbucket, api.AbstractApi);
  * @return {IThenable}
  */
 api.Bitbucket.prototype.getPullRequests = function() {
-	var url = 'repositories/' + this._owner + '/' + this._repoSlug + '/pullrequests/?state=merged,open';
-	return this.request(url, {'Authorization': 'Basic a2ljdW1raWN1bToxMXpobG01'})
-		.then(function(response) {
-			var pulls = response['values'];
-			return pulls.map(function(pull) {
-				return new models.bitbucket.PullRequest(pull);
+	var url = this._url + 'repositories/' + this._owner + '/' + this._repoSlug + '/pullrequests/?state=merged,open' +
+		'&pagelen=' + this._maxPageLength;
+	var pulls = [];
+	var httpHeader = {'Authorization': 'Basic a2ljdW1raWN1bToxMXpobG01'};
+
+	var request = function(url) {
+		return this
+			.request(url, httpHeader)
+			.then(function(response) {
+				var responsePulls = response['values'].map(function(pull) {
+					return new models.bitbucket.PullRequest(pull);
+				});
+
+				pulls = pulls.concat(responsePulls);
+				return response['next'];
+			})
+			.then(function(url) {
+				if (url) {
+					return request(url, httpHeader);
+				} else {
+					return pulls;
+				}
 			});
-		});
+	}.bind(this);
+
+	return request(url);
 };
