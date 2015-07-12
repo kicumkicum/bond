fbb2d699beca49c7b5725f0422aa887e870af163
kicumkicum <kicumkicum@gmail.com>
2015-07-12 23:58:10 +0600

    make branch-list

--- src/index.js
+++ src/index.js
@@ -20,10 +20,29 @@ var init = function() {
 			.then(goto);
 	});
 	document.getElementById('bit-branch').addEventListener('click', function() {
-		syncer.getBitbucketBranchesName();
-			//.then(function(all) {
-				//console.log(all);
-			//});
+		syncer.getBitbucketBranches()
+			.then(function(branches) {
+				var makeLi = function(ul, name, clickListener) {
+					var li = document.createElement("li");
+					li.onclick = clickListener;
+					li.appendChild(document.createTextNode(name));
+					ul.appendChild(li);
+				};
+
+				var ul = document.getElementById('content');
+				makeLi(ul, 'back', function() {
+					while (ul.lastChild) {
+						ul.removeChild(ul.lastChild);
+					}
+					ul.setAttribute('display', 'none');
+				});
+				branches.forEach(function(branch) {
+					makeLi(ul, branch.name, function() {
+						window.open(branch.links.html.href);
+					});
+				});
+				ul.style.display = 'block';
+			});
 	});
 	document.getElementById('bit-pull').addEventListener('click', function() {
 		syncer.getBitbucketPullRequestUrl()
