de5f70c239b0fbb3a0629426349f9e00cb6f039d
kicumkicum <kicumkicum@gmail.com>
2015-07-14 02:32:04 +0600

    add textarea for settings input and use it

--- src/options.js
+++ src/options.js
@@ -1,10 +1,10 @@
 // Saves options to chrome.storage
 function save_options() {
-	var color = document.getElementById('color').value;
-	var likesColor = document.getElementById('like').checked;
+	var settings = document.getElementById('settings').value;
+	//settings = JSON.parse(settings);
+
 	chrome.storage.sync.set({
-		favoriteColor: color,
-		likesColor: likesColor
+		'settings': settings
 	}, function() {
 		// Update status to let user know options were saved.
 		var status = document.getElementById('status');
@@ -19,12 +19,8 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
 	// Use default value color = 'red' and likesColor = true.
-	chrome.storage.sync.get({
-		favoriteColor: 'red',
-		likesColor: true
-	}, function(items) {
-		document.getElementById('color').value = items.favoriteColor;
-		document.getElementById('like').checked = items.likesColor;
+	chrome.storage.sync.get(null, function(items) {
+		document.getElementById('settings').value = items.settings;
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
