14d0a2b334ea6d3e08dd10c3d94521e0444ee6cf
kicumkicum <kicumkicum@gmail.com>
2015-07-15 00:17:18 +0600

    refactoring index.js. need more

--- src/index.js
+++ src/index.js
@@ -4,50 +4,58 @@
 goog.require('goog.base');
 goog.require('Syncer');
 
+/**
+ * @param {Array.<models.bitbucket.Branch|models.bitbucket.PullRequest>} elements
+ */
+var showList = function(elements) {
+	var makeLi = function(ul, name, clickListener) {
+		var li = document.createElement('li');
+		li.onclick = clickListener;
+		li.appendChild(document.createTextNode(name));
+		ul.appendChild(li);
+	};
+
+	var ul = document.getElementById('content');
+	makeLi(ul, 'back', function() {
+		while (ul.lastChild) {
+			ul.removeChild(ul.lastChild);
+		}
+		ul.setAttribute('display', 'none');
+	});
+	elements.forEach(function(element) {
+		makeLi(ul, element.name || element.title, function() {
+			window.open(element.links.html.href);
+		});
+	});
+	ul.style.display = 'block';
+};
+
+
+var goto = function(url) {
+	window.open(url);
+};
+
 var init = function() {
 	var syncer = new Syncer;
-	var goto = function(url) {
-		window.open(url);
-	};
 
 	chrome.tabs.getSelected(function(tab) {
 		var url = tab.url;
 		syncer.setUrl(url);
 	});
 
-	document.getElementById('redmine').addEventListener('click', function() {
-		syncer.getRedMineTicketUrl()
-			.then(goto);
-	});
-	document.getElementById('bit-branch').addEventListener('click', function() {
-		syncer.getBitbucketBranches()
-			.then(function(branches) {
-				var makeLi = function(ul, name, clickListener) {
-					var li = document.createElement("li");
-					li.onclick = clickListener;
-					li.appendChild(document.createTextNode(name));
-					ul.appendChild(li);
-				};
-
-				var ul = document.getElementById('content');
-				makeLi(ul, 'back', function() {
-					while (ul.lastChild) {
-						ul.removeChild(ul.lastChild);
-					}
-					ul.setAttribute('display', 'none');
-				});
-				branches.forEach(function(branch) {
-					makeLi(ul, branch.name, function() {
-						window.open(branch.links.html.href);
-					});
-				});
-				ul.style.display = 'block';
-			});
-	});
-	document.getElementById('bit-pull').addEventListener('click', function() {
-		syncer.getBitbucketPullRequestUrl()
-			.then(goto);
-	});
+	document.getElementById('redmine').onclick = syncer.goToRedmineTicket.bind(syncer);
+	document.getElementById('bit-pull').onclick = function() {
+		syncer.getBitbucketPullRequests().then(function(pulls) {
+			if (pulls.length === 1) {
+				goto(pulls[0].links.html.href);
+			} else {
+				showList(pulls);
+			}
+		});
+	};
+	document.getElementById('bit-branch').onclick = function() {
+		syncer.getBitbucketBranches().then(showList);
+	};
 
 	chrome.extension.onMessage.addListener(function(request, sender) {
 		if (request.action == "get-branches") {
