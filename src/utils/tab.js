df38b66ddb340b68e95172eb89dd885050c6e6c5
kicumkicum <kicumkicum@gmail.com>
2015-11-09 01:25:45 +0600

    Fix tab-utils

--- src/utils/tab.js
+++ src/utils/tab.js
@@ -11,12 +11,14 @@ utils.tab = {};
  */
 utils.tab.getChromeTabByUrl = function(url) {
 	return new Promise(function(resolve, reject) {
-		chrome.tabs.query({'windowId': 0}, function(tabs) {
-			var tab = tabs.filter(function(tab) {
-				return tab.url === url;
-			})[0];
+		chrome.windows.getCurrent(function(chromeWindow) {
+			chrome.tabs.query({'windowId': chromeWindow.id}, function(tabs) {
+				var tab = tabs.filter(function(tab) {
+					return tab.url === url;
+				})[0];
 
-			resolve(tab);
+				tab ? resolve(tab) : reject('not find tab by url: ' + url);
+			});
 		})
 	});
 };
@@ -24,9 +26,10 @@ utils.tab.getChromeTabByUrl = function(url) {
 
 /**
  * @param {utils.tab.Injections} injection
+ * @param {*} chromeTab
  * @return {IThenable.<*>}
  */
-utils.tab.inject = function(injection) {
+utils.tab.inject = function(injection, chromeTab) {
 	return new Promise(function(resolve, reject) {
 		var listener = function(request, sender) {
 			if (request.action === injection) {
@@ -36,7 +39,7 @@ utils.tab.inject = function(injection) {
 		};
 
 		chrome.extension.onMessage.addListener(listener);
-		chrome.tabs.executeScript(tab.id, {file: '/src/injections/' + injection + '.js'}, function() {
+		chrome.tabs.executeScript(chromeTab.id, {file: '/src/injections/' + injection + '.js'}, function() {
 			if (chrome.extension.lastError) {
 				var message = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
 				chrome.extension.onMessage.removeListener(listener);
