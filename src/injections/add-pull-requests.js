bcd55b5f3af322b27fae76aeaa50db8013fa29ca
kicumkicum <kicumkicum@gmail.com>
2016-05-29 23:16:21 +0600

    Add icon by state for pull requests

--- src/injections/add-pull-requests.js
+++ src/injections/add-pull-requests.js
@@ -12,15 +12,37 @@ var addPullrequests = function(pullrequests) {
  * @param {models.bitbucket.PullRequest} pullrequest
  */
 var addPullrequest = function(pullrequest) {
-	console.log('uo');
-	var li = document.createElement('li');
 	var a = document.createElement('a');
+	var li = document.createElement('li');
+	var div = document.createElement('div');
+	var image = document.createElement('img');
+
+	var imageName = '';
+	switch (pullrequest.state) {
+		case 'DECLINED':
+			imageName = 'false';
+			break;
+		case 'MERGED':
+			imageName = 'true';
+			break;
+		case 'OPEN':
+			imageName = 'ticket';
+			break;
+
+	}
+
+	image.setAttribute('src', '../images/' + imageName + '.png');
+	image.setAttribute('height', '10px');
+	image.style.marginRight = '5px';
 
 	a.setAttribute('class', 'query');
 	a.setAttribute('href', pullrequest.links.html.href);
 	a.innerText = pullrequest.title;
 
-	li.appendChild(a);
+	div.appendChild(image);
+	div.appendChild(a);
+
+	li.appendChild(div);
 	document.getElementById('sidebar').lastChild.appendChild(li);
 };
 
