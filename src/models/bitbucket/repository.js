927f5a46dad3eec9a1184ced4f79f1cc14d1fcec
kicumkicum <kicumkicum@gmail.com>
2016-06-13 16:12:02 +0600

    Inherit *models.bitbucket.Repository* from *models.Abstract*

--- src/models/bitbucket/repository.js
+++ src/models/bitbucket/repository.js
@@ -1,5 +1,7 @@
 goog.provide('models.bitbucket.Repository');
+goog.require('models.Abstract');
 goog.require('models.bitbucket.Links');
+
 
 
 /**
@@ -7,6 +9,15 @@ goog.require('models.bitbucket.Links');
  * @constructor
  */
 models.bitbucket.Repository = function(data) {
+	goog.base(this, data);
+};
+goog.inherits(models.bitbucket.Repository, models.Abstract);
+
+
+/**
+ * @override
+ */
+models.bitbucket.Repository.prototype.parse = function(data) {
 	this.links = new models.bitbucket.Links(data['links']);
 	this.type = data['type'];
 	this.name = data['name'];
